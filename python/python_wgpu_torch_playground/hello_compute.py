e6b42371d74dddd52a2215bf8390ffb106738df1
AsherJingkongChen <37398747+AsherJingkongChen@users.noreply.github.com>
2024-03-28 20:31:57 +0800

    (adjust): Bundle shader and driver code together

--- python/python_wgpu_torch_playground/hello_compute.py
+++ python/python_wgpu_torch_playground/hello_compute.py
@@ -11,6 +11,7 @@ def hello_compute():
     """
 
     import wgpu
+    from pathlib import Path
 
     """
     Define the number of elements, global and local sizes.
@@ -20,23 +21,13 @@ def hello_compute():
     local_size = [32, 1, 1]
     global_size = [n // local_size[0], 1, 1]
 
-    shader_source = f"""
-    @group(0) @binding(0)
-    var<storage,read> data1: array<i32>;
-
-    @group(0) @binding(1)
-    var<storage,read> data2: array<i32>;
-
-    @group(0) @binding(2)
-    var<storage,read_write> data3: array<i32>;
-
-    @compute
-    @workgroup_size({','.join(map(str, local_size))})
-    fn main(@builtin(global_invocation_id) index: vec3<u32>) {{
-        let i: u32 = index.x;
-        data3[i] = data1[i] + data2[i];
-    }}
-    """
+    shader_source = (
+        Path(__file__)
+        .with_name("shader.wgsl")
+        .open()
+        .read()
+        .format(",".join(map(str, local_size)))
+    )
 
     # Define two arrays
     data1 = memoryview(bytearray(n * 4)).cast("i")
@@ -49,7 +40,7 @@ def hello_compute():
     for i in range(n):
         data2[i] = i * 2
 
-    adapter = wgpu.gpu.request_adapter(power_preference="high-performance")
+    adapter = wgpu.gpu.request_adapter(power_preference="low-power")
 
     # Request a device with the timestamp_query feature, so we can profile our computation
     device = adapter.request_device(
@@ -175,6 +166,9 @@ def hello_compute():
 
     # Calculate the result on the CPU for comparison
     result_cpu = [a + b for a, b in zip(data1, data2)]
+
+    data1.release()
+    data2.release()
 
     # Ensure results are the same
     assert result == result_cpu
